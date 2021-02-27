9020e8fd810511f16f335c61736631137c6abdce
yezeahew <yezeahew@gmail.com>
2021-02-27 16:52:56 +0800

    test push to branch

--- client/src/components/Item/Item.js
+++ client/src/components/Item/Item.js
@@ -3,55 +3,77 @@ import {
     CardMedia,
     CardContent,
     Typography,
-    CardActionArea,
     Button,
+    Grid,
 } from "@material-ui/core";
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'; 
-import "./item.css";
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline'; import "./item.css";
 
-const Item = ({item}) => {
+const Item = ({ item, asRequest }) => {
     // console.log(`${JSON.stringify(item)}, ${item.item.name}`)
     return (
         <Card className="item-card">
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-                    height="260"
-                    className="card-image"
-                // image={product.media.source}
-                />
-                <CardContent className="content">
-                    <Typography
-                        className="title"
-                        gutterBottom
-                        variant="h5"
-                        component="h2"
-                    >
-                        {item.name}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-            <div className="content">
+            <CardMedia
+                component="img"
+                height="260"
+                className="card-image"
+            // image={product.media.source}
+            />
+            <CardContent className="content">
                 <Typography
+                    className="title"
+                    gutterBottom
+                    variant="h5"
+                    component="h2"
+                >
+                    {item.name}
+                </Typography>
+
+                {asRequest &&
+                    <>
+                        <Grid container item xs={12}>
+                            <Grid item xs={10}>
+                                <Typography
+                                    className="price"
+                                    gutterBottom
+                                    component="h2"
+                                >
+                                    {`Quantity Requested : 2`}
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={2}>
+                                <Typography
+                                    className="price"
+                                    gutterBottom
+                                    component="h2">
+                                    {`RM${item.price}`}
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </>
+
+
+                }
+
+                {!asRequest && <Typography
                     className="price"
                     gutterBottom
                     variant="h5"
                     component="h2"
                 >
                     {`RM${item.price}`}
-                </Typography>
-                <Button
-                    size="large"
-                    className="button"
-                    onClick={() => {
-                        console.log('button click')
-                    }}
-                >
-                    <ShoppingBasketIcon /> Add to basket
-              </Button>
-            </div >
-      </Card >
+                </Typography>}
+
+            </CardContent>
+
+            {!asRequest && <Button
+                size="large"
+                className="button"
+                onClick={() => { console.log('button click') }}
+            >
+                <AddCircleOutlineIcon /> Add to request
+              </Button>}
+        </Card >
     );
-  };
+};
 
 export default Item;
