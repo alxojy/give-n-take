38385b0f5b509ed4485787d4e6878c0d2d86496b
yezeahew <yezeahew@gmail.com>
2021-02-27 17:03:55 +0800

    Recode request details

--- client/src/components/Navigation/TemporaryTabs.js
+++ client/src/components/Navigation/TemporaryTabs.js
@@ -3,6 +3,7 @@ import { Tabs, Tab, AppBar } from "@material-ui/core";
 import RequestData from '../../dummyData/requestData.json';
 import ItemData from '../../dummyData/itemData.json';
 import Catalog from "../../components/Catalog";
+import RequestDetail from "../Request/RequestDetail";
 
 
 const TemporaryTabs = props => {
@@ -20,11 +21,13 @@ const TemporaryTabs = props => {
         <Tabs value={selectedTab} onChange={handleChange}>
           <Tab label="Item Catalog" />
           <Tab label="Request Catalog" />
+          <Tab label="Request Details" />
         </Tabs>
       </AppBar>
 
       {selectedTab === 0 && <Catalog type = {"item"} data = {ItemData.items}/>}
       {selectedTab === 1 && <Catalog type = {"request"} data = {RequestData.requests}/>}
+      {selectedTab === 2 && <RequestDetail request = {RequestData.request}/>}
     </>
   );
 };
