f8a1c225d9c565dc5be594b552f7895090bf3a76
Wei Ying Chia <Wei.Ying.Chia@wdc.com>
2021-02-28 10:08:40 +0800

    ADDED: user button on nav bar

--- client/src/components/Navigation/TemporaryTabs.js
+++ client/src/components/Navigation/TemporaryTabs.js
@@ -1,7 +1,7 @@
 import React,{useState, useEffect} from "react";
-import { Tabs, Tab, AppBar } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import { Tabs, Tab, AppBar, Toolbar, Typography, Button } from "@material-ui/core";
 import RequestData from '../../dummyData/requestData.json';
-import UserData from '../../hooks/UserAPI.js';
 import ItemData from '../../dummyData/itemData.json';
 import Catalog from "../../components/Catalog";
 import HomePage from "../../pages/HomePage";
@@ -14,10 +14,26 @@ import SignUpForm from "../Forms/SignUpForm";
 // Data
 import UserData from '../../hooks/UserAPI.js';
 
-const TemporaryTabs = props => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  tabs: {
+    flexGrow: 1,
+  },
+  user: {
+    flexGrow:1 ,
+    marginRight: '10px',
+  }
+}));
 
-const [selectedTab, setSelectedTab] = useState(0);
-const [userData, donationDetails, requestDetails] = UserData(null);
+const TemporaryTabs = props => {
+  const classes = useStyles();
+  const [selectedTab, setSelectedTab] = useState(0);
+  const [userData, donationDetails, requestDetails] = UserData(null);
 
 const handleChange = (event, newValue) => {
 // history.push(`/home/${tabNameToIndex[newValue]}`);
@@ -26,17 +42,33 @@ setSelectedTab(newValue);
 
 return (
 <>
-<AppBar position="static" color="transparent">
-<Tabs value={selectedTab} onChange={handleChange}>
-<Tab label="Home" />
-<Tab label="Item Catalog" />
-<Tab label="Requests List" />
-<Tab label="Request Details" />
-<Tab label="New Request Form" />
-<Tab label="User" />
-<Tab label="Sign Up Form" />
-</Tabs>
-</AppBar>
+<div className={classes.root}>
+  <AppBar position="static" color="transparent">
+    <Toolbar className={classes.tabs}>
+
+      <Tabs value={selectedTab} onChange={handleChange}>
+      <Tab label="Home" />
+      <Tab label="Item Catalog" />
+      <Tab label="Requests List" />
+      <Tab label="Request Details" />
+      <Tab label="New Request Form" />
+      <Tab label="Sign Up Form" value={6}/>
+      </Tabs>
+      
+      <Typography
+          className="title"
+          gutterBottom
+          variant="h5"
+          component="h2"
+          className={classes.user}
+      >
+      </Typography>
+      <Button color="inherit"
+        onClick={() => { setSelectedTab(5);}}>Welcome, {userData ? userData.name : null}</Button>
+    </Toolbar>
+  </AppBar>
+
+</div>
 
 {selectedTab === 0 && <HomePage />}
 {selectedTab === 1 && <Catalog type = {"item"} data = {ItemData.items}/>}
